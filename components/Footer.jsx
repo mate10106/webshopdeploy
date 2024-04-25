0a9c2f46dd6bd76500cc1cbdb9952c7dd078f1a5
Brösztl Máté 826 <bromat826@hengersor.hu>
2024-04-25 10:54:15 +0200

    change href

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 function Footer() {
   return (
     <div className="text-black bg-white w-full mt-8 rounded-xl text-center space-x-4">
-      <Link href="/aszf">Aszf</Link>
-      <Link href="/kapcsolat">Kapcsolat</Link>
+      <Link href="/Aszf">Aszf</Link>
+      <Link href="/Kapcsolat">Kapcsolat</Link>
       <p className="text-center p-2 font-light">
         Copyright © 2024 Webshop | Webshop. Minden jog fenntartva. All rights
         reserved.
